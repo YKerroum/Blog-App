99af5befdd14def20f5f44cf0214108a2326866e
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-03-03 22:13:05 +0100

    fix linters errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-
-  before do
+  before(:all) do
     @user = User.create(name: 'John Doe')
     @post = Post.create(author: @user, title: 'My Post')
   end
 
-  it "comment should update post comments counter" do
-    comment = Comment.create(author: @user, post: @post)
+  it 'comment should update post comments counter' do
+    Comment.create(author: @user, post: @post)
     expect(@post.comments_counter).to eq(1)
-
   end
 
-  it "comment should update post comments counter after destroy" do
+  it 'comment should update post comments counter after destroy' do
     comment = Comment.create(author: @user, post: @post)
     expect(@post.comments_counter).to eq(1)
 
@@ -21,7 +19,7 @@ RSpec.describe Comment, type: :model do
     expect(@post.comments_counter).to eq(0)
   end
 
-  it "comment should update post comments counter after update" do
+  it 'comment should update post comments counter after update' do
     comment = Comment.create(author: @user, post: @post)
     expect(@post.comments_counter).to eq(1)
 
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,33 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  it "update post likes counter" do
-    author = User.create(name: "John Doe")
-    post = Post.create(author: author, title: "My Post")
+  it 'update post likes counter' do
+    author = User.create(name: 'John Doe')
+    post = Post.create(author:, title: 'My Post')
 
-    like = Like.create(author: author, post: post)
+    Like.create(author:, post:)
     expect(post.likes_counter).to eq(1)
-
   end
 
-  it "destroy like updates post likes counter" do
-    author1 = User.create(name: "Mary Doe")
-    post = Post.create(author: author1, title: "My Post")
-    author = User.create(name: "John Doe")
+  it 'destroy like updates post likes counter' do
+    author1 = User.create(name: 'Mary Doe')
+    post = Post.create(author: author1, title: 'My Post')
+    author = User.create(name: 'John Doe')
 
-    like = Like.create(author: author, post: post)
+    like = Like.create(author:, post:)
     expect(post.likes_counter).to eq(1)
 
     like.destroy
     expect(post.likes_counter).to eq(0)
   end
 
-  it "update like updates post likes counter" do
-    author1 = User.create(name: "John Doe")
-    post = Post.create(author: author1, title: "My Post")
-    author2 = User.create(name: "Jane Doe")
+  it 'update like updates post likes counter' do
+    author1 = User.create(name: 'John Doe')
+    post = Post.create(author: author1, title: 'My Post')
+    author2 = User.create(name: 'Jane Doe')
 
-    like1 = Like.create(author: author1, post: post)
+    like1 = Like.create(author: author1, post:)
     expect(post.likes_counter).to eq(1)
 
     like1.update(author: author2)
