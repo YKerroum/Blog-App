c701389ae0bbc067234c136123e8f2c23f129c65
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-03-03 15:36:47 +0100

    add method to update comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,9 @@ class Comment < ApplicationRecord
     belongs_to :post,
         foreign_key: :post_id,
         class_name: :Post
+
+    def update_post_comments_counter
+        self.post.comments_counter = self.post.comments.count
+        self.post.save
+    end
 end
